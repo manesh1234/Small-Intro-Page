19c13ec1b3aad6d47e0e6c6a7a540f4e0d9670ff
manesh1234 <74581915+manesh1234@users.noreply.github.com>
2021-11-11 01:46:54 +0530

    Bug Fixes
    Bug Fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,14 @@
-if (!name) {
+/* JAVASCRIPT STARTS HERE */
+
+if(localStorage.getItem('name')==null){
     var name = prompt("Please Enter Your Name ");
-    localStorage.setItem('name', name);
+    while (name=="") {
+        name = prompt("Please Enter Your Name ");
+        localStorage.setItem('name', name);
+    }
 }
+
 document.querySelector('#uName').innerHTML = localStorage.getItem('name');
-document.querySelector('#hName').innerHTML = localStorage.getItem('name');
 var showMenu = (toggleId, navId) => {
     var toggle = document.getElementById(toggleId);
     var nav = document.getElementById(navId);
